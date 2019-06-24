1e615fd6ac47631a9f5c53a87ef7caef623ed204
BOB\Will <williamsolebo@gmail.com>
2019-06-24 15:37:40 +0100

    Updates

---
+++ precache-manifest.ca4902c0eaa3069a649d004736d7dc6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba36c585039f87966b511460e2faf27",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "52e2fae74434b8a96a1b",
+    "url": "/Todo-list/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "d371738cd90927e49773",
+    "url": "/Todo-list/static/js/2.71f2c0f4.chunk.js"
+  },
+  {
+    "revision": "52e2fae74434b8a96a1b",
+    "url": "/Todo-list/static/js/main.fe41853d.chunk.js"
+  },
+  {
+    "revision": "895e78035450c62a9efe",
+    "url": "/Todo-list/static/js/runtime~main.9eebd9a7.js"
+  }
+]);
